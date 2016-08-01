dd301a1c6d8a322dc8777718a1e6a9bb26d9f10d
Eric Fenderbosch <eric@fender.net>
2016-08-01 14:16:30 -0400

    auditing almost working

--- src/main/java/net/fender/springboot/example/Campaign.java
+++ src/main/java/net/fender/springboot/example/Campaign.java
@@ -1,5 +1,8 @@
 package net.fender.springboot.example;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -7,13 +10,34 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 @Entity
 @Table(name = "campaigns")
+@JsonInclude(NON_EMPTY)
+/*
+ * CREATE TABLE campaigns ( id bigserial NOT NULL, description text, created
+ * timestamp without time zone, updated timestamp without time zone, version
+ * integer, CONSTRAINT campaigns_pkey PRIMARY KEY (id)); ALTER TABLE campaigns
+ * OWNER TO pwruser; GRANT ALL ON TABLE campaigns TO pwruser; GRANT SELECT,
+ * UPDATE, INSERT, DELETE ON TABLE campaigns TO appuser;
+ */
 public class Campaign {
 
 	@Id
+	// serialize as String because of JavaScript's 52-bit mantissa
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Long id;
 	private String description;
+	@CreatedDate
+	private ZonedDateTime created;
+	@LastModifiedDate
+	private ZonedDateTime updated;
 	@Version
 	private int version;
 
@@ -31,6 +55,22 @@ public class Campaign {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public ZonedDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(ZonedDateTime created) {
+		this.created = created;
+	}
+
+	public ZonedDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(ZonedDateTime updated) {
+		this.updated = updated;
 	}
 
 	public int getVersion() {
--- src/main/java/net/fender/springboot/example/ExampleSpringBootService.java
+++ src/main/java/net/fender/springboot/example/ExampleSpringBootService.java
@@ -2,14 +2,23 @@ package net.fender.springboot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.CurrentDateTimeProvider;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class ExampleSpringBootService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringBootService.class, args);
 	}
 
+	@Bean
+	public DateTimeProvider dateTimeProvider() {
+		return CurrentDateTimeProvider.INSTANCE;
+	}
 }
